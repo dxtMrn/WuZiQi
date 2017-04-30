dd4a18f7822f261756a01bfb2c12ae9389b4e8b5
dxtMrn <1683884853@qq.com>
2017-04-30 22:09:52 +0800

    Initial commit

---
+++ app/src/main/java/com/apress/gerber/wuziqi/MainActivity.java
@@ -0,0 +1,36 @@
+package com.apress.gerber.wuziqi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WuZiQiPanel wuziqiPanal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wuziqiPanal = (WuZiQiPanel) findViewById(R.id.id_wuziqi);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if(id == R.id.action_settings){
+            wuziqiPanal.restart();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
